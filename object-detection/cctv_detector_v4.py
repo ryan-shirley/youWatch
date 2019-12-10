09ea20063d30f9717d8f30c6c022162cb1aea50d
Ryan Shirley <ryanjshirley@gmail.com>
2019-12-10 22:39:44 +0000

    Analyse video and save to gif

--- object-detection/cctv_detector_v4.py
+++ object-detection/cctv_detector_v4.py
@@ -2,7 +2,7 @@
 import cv2 
 import os 
 
-# Import for image to gif conversion
+# Import for image to gif conversion and optimisation
 import imageio
 
 # Object detection importing
@@ -18,17 +18,38 @@ from video import Video
 # Track time for how long to execute
 import time
 
+# Initialize Cloud Firestore
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore
+
+# Use a service account
+cred = credentials.Certificate('./youwatch-homecctv-21bf4eac8f29.json')
+firebase_admin.initialize_app(cred)
+
+db = firestore.client()
+
 # Locations
 inputVideoPath = "./data/input-video/"
 personFoundPath = "./data/person-found/"
 personNotFoundPath = "./data/person-not-found/"
 noDetectionsPath = "./data/no-detections/"
+gifOutputPath = "./data/gif-thumbnails/"
+
+tempImagesPath = "./data/temp-images/"
+tempImagesOutputPath = "./data/temp-images-output/"
 
 analysedVideosPath = "./data/analysed-videos/"
 
 model_path = "./models/yolo.h5"
 
-start_time = ''
+# Resize (resolution / resDownScale) & Compression (80 = low | 200 = high)
+resDownScale = 4
+compression = '200'
+secondsAfter = 5 * 2
+
+# Timer
+start_time = time.time()
 
 # Settings
 finishAfterPersonFound = True
@@ -39,8 +60,103 @@ detector.setModelTypeAsYOLOv3()
 detector.setModelPath(model_path)
 detector.loadModel()
 
+
+# Generate frame from video
+def generateFrame(cam, currentframe):
+    # reading from frame 
+    ret,frame = cam.read() 
+
+    if ret: 
+        # if video is still left continue creating images 
+        name = tempImagesPath + 'frame' + str(currentframe) + '.jpg'
+
+        # Resize to 720P
+        height , width , layers =  frame.shape
+        new_h= round(height/resDownScale)
+        new_w= round(width/resDownScale)
+        resize = cv2.resize(frame, (new_w, new_h))
+        # resize = cv2.resize(frame, (1280, 720))
+        cv2.imwrite(name, resize) 
+
+        # Log
+        # print("Video frame converted to .jpg - Path: " + name)
+
+        return name
+        
+    else: 
+        return False
+
+# Analyse individual frame
+def analyseFrame(frame, detector):
+
+    try: 
+        # creating a folder named data 
+        if not os.path.exists(tempImagesOutputPath): 
+            os.makedirs(tempImagesOutputPath) 
+
+    # if not created then raise error 
+    except OSError: 
+        print ('Error: Creating directory of ' + tempImagesOutputPath) 
+
+    input_path = frame
+
+    # Replace original temp path to output temp path
+    output_path = input_path.replace(tempImagesPath,tempImagesOutputPath)
+
+    # print("Saving analysed frame.")
+
+    detection = detector.detectObjectsFromImage(input_image=input_path, output_image_path=output_path)
+
+    # Loop through detections found
+    detections = []
+    for eachItem in detection:
+        detections.append(eachItem["name"])
+        # print(eachItem["name"] , " : ", eachItem["percentage_probability"])
+    
+    return detections
+
+# Convert the images into animated gif
+def convertImagesToGif(frames, name):
+    # Check temp folder exists
+        try: 
+            # creating a folder named data 
+            if not os.path.exists(tempImagesPath): 
+                os.makedirs(tempImagesPath) 
+                print ('Creating directory of ' + tempImagesPath) 
+        # if not created then raise error 
+        except OSError: 
+            print ('Error: Creating directory of ' + tempImagesPath) 
+
+        images = []
+        for filename in frames:
+            images.append(imageio.imread(filename))
+
+        # Save Gif
+        print('Saving gif.')
+        gifName = name.replace('mp4', 'gif')
+        imageio.mimsave(gifOutputPath + gifName, images)
+
+        # Optimise Gif
+        os.system("./gifsicle -O3 --lossy=" + compression + " --colors 256 -o " + gifOutputPath + gifName + " " + gifOutputPath + gifName)
+
+        return 'converted'
+
+# Remove all files in temp folders
+def cleanTemporaryFiles():
+    # Clean original
+    originalfilelist = [ f for f in os.listdir(tempImagesPath) if f.endswith(".jpg") ]
+    for f in originalfilelist:
+        os.remove(os.path.join(tempImagesPath, f))
+    
+    # Clean analysed
+    analysedfilelist = [ f for f in os.listdir(tempImagesOutputPath) if f.endswith(".jpg") ]
+    for f in analysedfilelist:
+        os.remove(os.path.join(tempImagesOutputPath, f))
+
 # Analyse video file for person
 def analyseVideo(video):
+    print('Starting to analyse video for person')
+
     # Read the video from specified path
     pathToVideo = inputVideoPath + video.originalFile 
     cam = cv2.VideoCapture(pathToVideo) 
@@ -62,9 +178,7 @@ def analyseVideo(video):
     while(True): 
 
         # Send to get indiviudal frame
-        generatedFrame = video.generateFrame(cam, currentframe)
-
-        break
+        generatedFrame = generateFrame(cam, currentframe)
 
         # Check if frame was found and generated
         if generatedFrame:
@@ -72,7 +186,7 @@ def analyseVideo(video):
             video.originalFrames.append(generatedFrame)
 
             # Analyse Frame
-            detections = video.analyseFrame(generatedFrame, detector)
+            detections = analyseFrame(generatedFrame, detector)
             
             # Add detections to video object
             for d in detections:
@@ -83,17 +197,23 @@ def analyseVideo(video):
             # If person found
             if finishAfterPersonFound and "person" in video.detections:
                 video.framesForGif.append(generatedFrame)
-                print("Found a person now im generating few more frames for gif!")
+                print("Found a person! Now generating few more frames for gif!")
 
-                for i in range(5 * video.fps):
+                for i in range(secondsAfter):
+                    
+                    # Skip frames - Get one per second
+                    for j in range(round(video.fps / 2)):
+                        ret,frame = cam.read() 
+
                     # Generate Frame
-                    frame = video.generateFrame(cam, currentframe)
+                    frame = generateFrame(cam, currentframe)
+                    currentframe += 1
 
                     # Save frame to list to save as gif
-                    video.framesForGif.append(fame)
+                    video.framesForGif.append(frame)
 
-                # TODO: Save Images into animated gif
-                video.convertImagesToGif()
+                # Save Images into animated gif
+                convertImagesToGif(video.framesForGif, video.originalFile)
 
                 break
 
@@ -101,13 +221,23 @@ def analyseVideo(video):
             # show how many frames are created 
             currentframe += 1
         else:
+            print("No person was found!")
             break
 
     # Release all space and windows once done 
     cam.release() 
     cv2.destroyAllWindows() 
 
-    print("Time taken: %s seconds." % (time.time() - start_time))
+    # Clean up the temp files
+    cleanTemporaryFiles()
+
+    # Insert doc into firestore
+    ts = time.gmtime()
+    db.collection(u'detections').add({
+        u'camera': u'Camera Name',
+        u'time-taken': round(time.time() - start_time) + ' seconds',
+        u'timestamp': time.strftime("%B %d, %Y at %I:%M:%S %p GMT", ts)
+    })
 
 # Single Analysis of video
-analyseVideo(Video("2019-11-18T16-27-35.mp4"))
+analyseVideo(Video("TEST-CCCTV.mp4"))
